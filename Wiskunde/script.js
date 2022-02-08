413d1d3894af9b03cc07f03641fcc21d44bdfe0f
Thomas1070 <95695250+Thomas1070@users.noreply.github.com>
2022-02-08 16:10:12 +0100

    safe

--- Wiskunde/script.js
+++ Wiskunde/script.js
@@ -1,4 +1,9 @@
 let getal1 = prompt('geef het eerste getal');
 let getal2 = prompt('geef het 2e getal');
-let som = getal1 + getal2;
-alert(som);
+
+let isGetal1KleinerDanGetal2 = getal1 < getal2;
+if(isGetal1KleinerDanGetal2){
+    alert('Getal 1 is kleiner dan getal 2');
+} else{
+    alert('Getal 1 is groter dan getal 2')
+}
