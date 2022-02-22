ee23237a7434ddcb7e2528d3fd1a5733428d193b
Thomas1070 <95695250+Thomas1070@users.noreply.github.com>
2022-02-22 16:22:29 +0100

    Update script.js

--- zwart-wit-deel-2/script.js
+++ zwart-wit-deel-2/script.js
@@ -4,6 +4,31 @@ const selectVoorTekstKleur = document.querySelector('#tekstkleur');
   const html = document.querySelector('html');
   document.body.style.padding = '10px';
 
+  const select = document.querySelector('select');
+const html = document.querySelector('html');
+document.body.style.padding = '10px';
+
+function updateBackgroundAndTextColor(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.onchange = function () {
+  console.log('huidige waarde van de select / dropdown:', select.value);
+  if (select.value === 'black') {
+    updateBackgroundAndTextColor('black', 'white');
+  }
+  else if (select.value === 'white') {
+    updateBackgroundAndTextColor('white', 'black');
+  }
+  if (select.value === 'Red'){
+    updateBackgroundAndTextColor('Red', 'White');
+  }
+  else if (select.value === 'Blue') {
+    updateBackgroundAndTextColor('Blue', 'White');
+  }
+}
+
   /**
    * deze functie gaat de tekstkleur aanpassen
    * @param {*} textColor 
