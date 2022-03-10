97212f7aced58629d7a4beb7bdc846ae1d041350
Thomas1070 <95695250+Thomas1070@users.noreply.github.com>
2022-03-10 11:32:25 +0100

    pORTFOLIO

--- Print-Lijst/script.js
+++ Print-Lijst/script.js
@@ -1,8 +1,17 @@
-var auto1 = {Merk:"Mercedes", motor:"Benzine", Model:"A-Klasse"};
-var auto2 = {Merk:"Audi", motor:"Benzine", Model:"A3"};
-var auto3 = {Merk:"BMW", motor:"Diesel", Model:"Serie 1"};
+let auto1 = {Merk:"Mercedes", motor:"Benzine", Model:"A-Klasse"};
+let auto2 = {Merk:"Audi", motor:"Benzine", Model:"A3"};
+let auto3 = {Merk:"BMW", motor:"Diesel", Model:"Serie 1"};
+let autos = [auto1, auto2, auto3];
 
-var LijstObjecten = [auto1, auto2, auto3]
-for (let object of LijstObjecten) {
-    console.log(object);
+console.log(autos.lenght);
+
+for (let i = 0; i < autos.length; i++) {
+    objectPrinten(autos[i]);
 }
+
+function objectPrinten(parameter) {
+    console.log(parameter.Merk + ' ' + parameter.motor + ' ' + parameter.Model);
+
+    
+}
+
